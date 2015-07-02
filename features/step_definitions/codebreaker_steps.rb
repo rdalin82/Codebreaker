2ff888fa883a07e7f6105373ae540057ad375f43
rdalin82 <Robert.Dalin@gmail.com>
2015-07-02 09:44:03 -0500

    start of chap6

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -16,6 +16,13 @@ Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do 
-	game = CodeBreaker::Game.new(output)
+	@messenger = StringIO.new
+	#replace output with @messenger
+	game = CodeBreaker::Game.new(@messenger)
 	game.start
 end
+
+Given /^the secret code is "([^'']*)"$/ do [secret]
+	game = Codebreaker::Game.new(output)
+	game.start(secret)
+end 
