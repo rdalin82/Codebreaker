678a6b987345344225e1f83fd2d897703cb5b7ee
rdalin82 <Robert.Dalin@gmail.com>
2015-07-05 10:09:43 -0500

    added separate exact_match_count method

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -10,21 +10,26 @@ module CodeBreaker
 		end
 		
 		def guess(guess)
-			exact_match_count = 0
+			
 			number_match_count = 0
-			mark = ''
-			(0..3).each do |index|
-				if exact_match?(guess, index)
-					exact_match_count += 1 
-				end 
-			end 
 			(0..3).each do |index|
 				if number_match?(guess, index)
-					number_match_count += 1
+					number_match_count += 1 
+				end 
+			end 
+			
+			@output.puts '+'*exact_match_count(guess) + '-'*number_match_count
+		end
+
+		def exact_match_count(guess)
+			exact_match_count = 0
+			(0..3).each do |index|
+				if exact_match?(guess, index)
+					exact_match_count += 1
 				end
 			end 
-			@output.puts '+'*exact_match_count + '-'*number_match_count
-		end
+			exact_match_count
+		end 
 		def exact_match?(guess, index)
 			guess[index] == @secret[index]
 		end 
