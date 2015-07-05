b29c7b153c32e8ab0d2d844baa137a46a2208b8c
rdalin82 <Robert.Dalin@gmail.com>
2015-07-05 10:47:20 -0500

    replaced do method with inject method

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -10,26 +10,34 @@ module CodeBreaker
 		end
 		
 		def guess(guess)
-			@output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
+			marker = Marker.new(@secret,guess)
+			@output.puts '+'*marker.exact_match_count() + '-'*marker.number_match_count()
 		end
+	end 
 
-		def exact_match_count(guess)
+	class Marker
+		def initialize(secret, guess)
+			@secret, @guess = secret, guess
+		end 
+
+		def exact_match_count()
 			(0..3).inject(0) do |count, index|
-				count + (exact_match?(guess, index) ? 1 : 0)
+				count + (exact_match?(index) ? 1 : 0)
 			end 
 		end 
-		def number_match_count(guess)
+
+		def number_match_count()
 			(0..3).inject(0) do |count, index| 
-				count + (number_match?(guess, index) ? 1 : 0)
+				count + (number_match?(index) ? 1 : 0)
 			end 
 		end 
-		def exact_match?(guess, index)
-			guess[index] == @secret[index]
+
+		def exact_match?(index)
+			@guess[index] == @secret[index]
 		end 
-		def number_match?(guess, index)
-			@secret.include?(guess[index]) && !exact_match?(guess, index)
+
+		def number_match?(index)
+			@secret.include?(@guess[index]) && !exact_match?(index)
 		end 
 	end
-
-
 end
